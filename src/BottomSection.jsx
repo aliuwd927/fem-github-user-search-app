7f470dd0fb1a0d03cb8e6556a5b314a590ee6b1a
Andy Liu <aliu927@gmail.com>
2022-01-20 23:31:29 -0800

    updated

--- src/BottomSection.jsx
+++ src/BottomSection.jsx
@@ -5,7 +5,128 @@ import WebsiteImg from "./assets/icon-website.svg";
 import TwitterImg from "./assets/icon-twitter.svg";
 
 function BottomSection({userData}){
-  //console.log(userData.name)
+  console.log(Object.keys(userData).length);
+
+  const ifshitnotvalid =()=>{
+
+    return(
+    
+      <div className ='UserProfileInformationContainer'>
+
+      <div className="UserProfile_Header">
+        <h4>The Octocat</h4>
+        <p>Joined 25 Jan 2011</p>
+      </div>
+
+      <div className="UserProfile_Handle">
+        <p>@octocat</p>
+      </div>
+
+      <div className="UserProfile_Bio">
+        <p>This Profile has no bio</p>
+      </div>
+
+      <div className="UserProfile_RepoFollow">
+        <div className='RepoContainer'>
+          <h5>Repos</h5>
+          <h3>8</h3>
+        </div>
+        <div className='FollowerContainer'>
+          <h5>Followers</h5>
+          <h3>3938</h3>
+        </div>
+        <div className="FollowingContainer">
+          <h5>Following</h5>
+          <h3>9</h3>
+        </div>
+      </div>
+
+      <div className="UserProfile_LocationSocialMedia_Container">
+       <div className="UserProfile_LocationSocialMedia_Left">
+         <div className="UserProfile_LocationSocialMedia_Location">
+           <img src={LocationImg} alt="" title="Location"/>
+           <p>San Francisco</p>
+         </div>
+         <div className="UserProfile_LocationSocialMedia_Website">
+           <img src={WebsiteImg} alt="" title="Website"/>
+           <p>https://github.blog</p>
+         </div>
+       </div>
+       <div className="UserProfile_LocationSocialMedia_Right">
+         <div className="UserProfile_LocationSocialMedia_Twitter">
+           <img src={TwitterImg} alt="" title="Twitter"/>
+           <p>Not Availiable</p>
+         </div>
+         <div className="UserProfile_LocationSocialMedia_Company">
+           <img src={CompanyImg} alt="" title="Company"/>
+           <p>@github</p>
+         </div>
+       </div>
+      </div>
+    </div>
+    );
+  }
+
+  const ifshitisvalid = () =>{
+    return(
+      <div className ='UserProfileInformationContainer'>
+
+      <div className="UserProfile_Header">
+        <h4>{userData.login}</h4>
+        <p>Joined {userData.created_at}</p>
+      </div>
+
+      <div className="UserProfile_Handle">
+        <p>@{userData.login}</p>
+      </div>
+
+      <div className="UserProfile_Bio">
+        <p>{userData.bio}</p>
+      </div>
+
+      <div className="UserProfile_RepoFollow">
+        <div className='RepoContainer'>
+          <h5>Repos</h5>
+          <h3>{userData.public_repos}</h3>
+        </div>
+        <div className='FollowerContainer'>
+          <h5>Followers</h5>
+          <h3>{userData.followers}</h3>
+        </div>
+        <div className="FollowingContainer">
+          <h5>Following</h5>
+          <h3>{userData.following}</h3>
+        </div>
+      </div>
+
+      <div className="UserProfile_LocationSocialMedia_Container">
+       <div className="UserProfile_LocationSocialMedia_Left">
+         <div className="UserProfile_LocationSocialMedia_Location">
+           <img src={LocationImg} alt="" title="Location"/>
+           <p>{userData.location}</p>
+         </div>
+         <div className="UserProfile_LocationSocialMedia_Website">
+           <img src={WebsiteImg} alt="" title="Website"/>
+           <p>{userData.url}</p>
+         </div>
+       </div>
+       <div className="UserProfile_LocationSocialMedia_Right">
+         <div className="UserProfile_LocationSocialMedia_Twitter">
+           <img src={TwitterImg} alt="" title="Twitter"/>
+           <p>{userData.twitter_username}</p>
+         </div>
+         <div className="UserProfile_LocationSocialMedia_Company">
+           <img src={CompanyImg} alt="" title="Company"/>
+           <p>{userData.company}</p>
+         </div>
+       </div>
+      </div>
+
+      
+    </div>
+  
+    )
+  };
   return(
     <div className="BottomSection">
     {/*LEFT SIDE */}
@@ -14,11 +135,93 @@ function BottomSection({userData}){
       </div>
 
 
-        {/*RIGHT SIDE */}
-      <div className ='UserProfileInformationContainer'>
+    {Object.keys(userData).length === 0 ? ifshitnotvalid() : ifshitisvalid()}
+
+    </div>//End of BottomSection Div
+  );
+}
+
+export default BottomSection;
+
+/* 
+Step 1:
+Figure out how to set a default child
+
+Step 2:
+Render Child if username is valid.
+
+
+if(objisempty.length = 0)
+const ifshitnotvalid =()=>{
+
+return(
+
+<div className ='UserProfileInformationContainer'>
 
         <div className="UserProfile_Header">
-          <h4>The Octocat</h4>
+          <h4>{userData.login}</h4>
+          <p>Joined {userData.created_at}</p>
+        </div>
+
+        <div className="UserProfile_Handle">
+          <p>@{userData.login}</p>
+        </div>
+
+        <div className="UserProfile_Bio">
+          <p>{userData.bio}</p>
+        </div>
+
+        <div className="UserProfile_RepoFollow">
+          <div className='RepoContainer'>
+            <h5>Repos</h5>
+            <h3>{userData.public_repos}</h3>
+          </div>
+          <div className='FollowerContainer'>
+            <h5>Followers</h5>
+            <h3>{userData.followers}</h3>
+          </div>
+          <div className="FollowingContainer">
+            <h5>Following</h5>
+            <h3>{userData.following}</h3>
+          </div>
+        </div>
+
+        <div className="UserProfile_LocationSocialMedia_Container">
+         <div className="UserProfile_LocationSocialMedia_Left">
+           <div className="UserProfile_LocationSocialMedia_Location">
+             <img src={LocationImg} alt="" title="Location"/>
+             <p>{userData.location}</p>
+           </div>
+           <div className="UserProfile_LocationSocialMedia_Website">
+             <img src={WebsiteImg} alt="" title="Website"/>
+             <p>{userData.url}</p>
+           </div>
+         </div>
+         <div className="UserProfile_LocationSocialMedia_Right">
+           <div className="UserProfile_LocationSocialMedia_Twitter">
+             <img src={TwitterImg} alt="" title="Twitter"/>
+             <p>{userData.twitter_username}</p>
+           </div>
+           <div className="UserProfile_LocationSocialMedia_Company">
+             <img src={CompanyImg} alt="" title="Company"/>
+             <p>{userData.company}</p>
+           </div>
+         </div>
+        </div>
+
+        
+      </div>
+
+);
+
+:
+
+const ifshitisvalid = (){
+  return(
+    <div className ='UserProfileInformationContainer'>
+
+        <div className="UserProfile_Header">
+          <h4>{userData.userName}</h4>
           <p>Joined 25 Jan 2011</p>
         </div>
 
@@ -69,10 +272,130 @@ function BottomSection({userData}){
         </div>
 
         
-      </div>{/* End of User Profile Container */}
+      </div>
 
-    </div>//End of BottomSection Div
-  );
+  )
 }
 
-export default BottomSection;
+
+
+
+
+
+
+
+
+
+
+Revert to default.
+ <div className ='UserProfileInformationContainer'>
+
+ <div className="UserProfile_Header">
+   <h4>The Octocat</h4>
+   <p>Joined 25 Jan 2011</p>
+ </div>
+
+ <div className="UserProfile_Handle">
+   <p>@octocat</p>
+ </div>
+
+ <div className="UserProfile_Bio">
+   <p>This Profile has no bio</p>
+ </div>
+
+ <div className="UserProfile_RepoFollow">
+   <div className='RepoContainer'>
+     <h5>Repos</h5>
+     <h3>8</h3>
+   </div>
+   <div className='FollowerContainer'>
+     <h5>Followers</h5>
+     <h3>3938</h3>
+   </div>
+   <div className="FollowingContainer">
+     <h5>Following</h5>
+     <h3>9</h3>
+   </div>
+ </div>
+
+ <div className="UserProfile_LocationSocialMedia_Container">
+  <div className="UserProfile_LocationSocialMedia_Left">
+    <div className="UserProfile_LocationSocialMedia_Location">
+      <img src={LocationImg} alt="" title="Location"/>
+      <p>San Francisco</p>
+    </div>
+    <div className="UserProfile_LocationSocialMedia_Website">
+      <img src={WebsiteImg} alt="" title="Website"/>
+      <p>https://github.blog</p>
+    </div>
+  </div>
+  <div className="UserProfile_LocationSocialMedia_Right">
+    <div className="UserProfile_LocationSocialMedia_Twitter">
+      <img src={TwitterImg} alt="" title="Twitter"/>
+      <p>Not Availiable</p>
+    </div>
+    <div className="UserProfile_LocationSocialMedia_Company">
+      <img src={CompanyImg} alt="" title="Company"/>
+      <p>@github</p>
+    </div>
+  </div>
+ </div>
+
+ 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
